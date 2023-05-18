23c5d26cfef732bacd09ee5c988c2fcd7d050d51
=GymnastiarR <=>
2023-05-18 16:24:23 +0700

    test

--- filter.js
+++ filter.js
@@ -1,24 +1,69 @@
 function filterProducts() {
   // Get the selected filter values
-  const filters = Array.from(document.querySelectorAll('input[type="checkbox"]:checked')).map(checkbox => checkbox.value);
+  const filters = Array.from( document.querySelectorAll( 'input[type="checkbox"]:checked' ) ).map( checkbox => checkbox.value );
 
-  // Get all the product items
-  const products = document.querySelectorAll('.pro');
+  if ( !filters[ 0 ] ) {
+    rendering( products );
+    return;
+  }
 
-  // Loop through the product items and show/hide based on filters
-  products.forEach(product => {
-    const productCategories = product.getAttribute('data-categories').split(',');
+  const selectedProduct = products.filter( ( product ) => {
+    if ( filters.includes( product.kategori ) ) {
+      return product;
+    }
+  } );
 
-    // Check if the product has any of the selected categories
-    const hasSelectedCategories = filters.some(category => productCategories.includes(category));
-
-    // Show/hide the product's div based on the selected categories
-    product.style.display = hasSelectedCategories ? 'block' : 'none';
-  });
+  rendering( selectedProduct );
 }
 
-const filterForm = document.getElementById('filterForm');
-filterForm.addEventListener('submit', event => {
+const filterForm = document.getElementById( 'filterForm' );
+filterForm.addEventListener( 'submit', event => {
   event.preventDefault();
   filterProducts();
-});
+} );
+
+function rendering( products ) {
+  const container = document.querySelector( '.pro-container' );
+  container.innerHTML = '';
+
+  products.forEach( ( product ) => {
+    const divProduct = document.createElement( 'div' );
+    divProduct.classList.add( 'pro' );
+    divProduct.setAttribute( 'data-categories', `${product.kategori}` );
+    const data = `
+  <img src="${product.img}" alt="" />
+  <div class="des">
+    <span>${product.merek}</span>
+    <h5>${product.name}</h5>
+    <div class="star">
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+    </div>
+    <h4>Rp 7.000.000</h4>
+  </div>
+  <a href="#"><i class="fas fa-shopping-cart cart"></i></a>
+  `;
+    divProduct.innerHTML = data;
+    container.appendChild( divProduct );
+  } );
+}
+
+const products = [
+  {
+    name: 'EVGA Geforce-GTX-1080-11GB',
+    merek: 'nvidia',
+    img: 'img/EVGA Geforce-GTX-1080-11GB.jpg',
+    kategori: 'VGA'
+  },
+  {
+    name: 'EVGA Geforce-GTX-1080-11GB',
+    merek: 'nvidia',
+    img: 'img/EVGA Geforce-GTX-1080-11GB.jpg',
+    kategori: 'Processor'
+  },
+];
+
+rendering( products );
